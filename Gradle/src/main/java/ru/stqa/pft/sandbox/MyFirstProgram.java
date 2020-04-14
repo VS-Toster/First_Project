7f260ae2497b45867a336b57267eecccb7e4f1d5
VS-Toster <drumtrumparam@gmail.com>
2020-04-14 23:03:02 +0300

    Добавленно несколько функций.

--- Gradle/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Gradle/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,42 @@
 package ru.stqa.pft.sandbox;
 
-public class MyFirstProgram {
-   public static void main(String[] args) {
-       System.out.println ("Hello World"); }
+import java.sql.SQLOutput;
 
-  }
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        double ara = 5;
+        double bara = 4;
+        double cara = 3;
+        System.out.println( "Площадь куба составит:" + " "+ areaOfCube(ara,bara,cara));
+    }
+/*
+        rout();
+
+    }
+
+        public static void rout() {
+
+            int wayOne = 26;
+            int wayTwo = 30;
+            double wayThree =10.5;
+            double mainWay = wayOne + wayTwo + wayThree;
+            System.out.println("Ваш путь составит:" + mainWay +" "+ "киллометров");
+
+
+*/
+
+
+
+
+
+            public static double  areaOfCube (double a, double b, double c) {
+                return a + 2*b * c;
+
+            }
+
+
+
+
+
+
+        }
